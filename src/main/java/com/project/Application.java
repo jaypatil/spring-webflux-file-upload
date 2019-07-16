912cf8bff2ac901dfa08f7c295bf69041a396680
Jay Kumar Patil <jaypatil.sonu@gmail.com>
2019-07-16 11:49:47 +0530

    Intilize file upload

---
+++ src/main/java/com/project/Application.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//https://github.com/hantsy
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
